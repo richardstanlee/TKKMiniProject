9fc5e0cd08b3936002a0cce01f75f4b1167e1cfe
richardstanlee <richard,stanlee@binus.ac.id>
2021-04-27 17:12:17 +0700

    inisialisasi_awal_miniProject

---
+++ src/main/java/org/bsim/exam/tkk/TkkApplication.java
@@ -0,0 +1,13 @@
+package org.bsim.exam.tkk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TkkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TkkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/bsim/exam/tkk/TkkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bsim.exam.tkk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TkkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
